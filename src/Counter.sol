ab2d36ecefb470cab005eae6a26a83e2db231d79
lucas-manuel <lucas@maple.finance>
2022-10-14 15:43:41 -0500

    feat: update to add basic log issue

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
 
 contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    uint256 public number;
 
     function increment() public {
         number++;
     }
+
 }
